9c8679e9d791830ca8761dc9f49df1052f8679f8
fish <diamond.zuo@gmail.com>
2015-07-09 13:44:02 +0800

    add icons.html

--- js/app.js
+++ js/app.js
@@ -109,6 +109,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                     }
                 }
             })
+            .state('app.icons', {
+                url: "/icons",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/icons.html"
+                    }
+                }
+            })
         ;
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/header');
