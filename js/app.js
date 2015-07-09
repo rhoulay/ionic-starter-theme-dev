12cc43ae08c03e9833dc87191caa82e3c4132eeb
fish <diamond.zuo@gmail.com>
2015-07-09 13:56:22 +0800

    add buttons-headers.html & changed view-title of some buttons templates

--- js/app.js
+++ js/app.js
@@ -117,6 +117,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                     }
                 }
             })
+            .state('app.buttonsHeaders', {
+                url: "/buttons-headers",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/buttons-headers.html"
+                    }
+                }
+            })
         ;
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/header');
